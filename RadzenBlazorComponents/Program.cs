cbcda2b94b968e7cf0ddf895baa83466d810b447
Sundaram Subramanian <learnwithsundar@gmail.com>
2021-02-28 02:05:54 +0530

    Initial Commit
    Initial Commit - Setting up the code base

---
+++ RadzenBlazorComponents/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Radzen;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadzenBlazorComponents
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
